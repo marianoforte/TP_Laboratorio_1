de37c394e379f6622143689fb8c878a17e50da79
marianoforte <marianoforte92@gmail.com>
2020-09-12 00:39:53 -0300

    Version 3.6

---
+++ TP 1_Calculadora/CalculosAritmeticos.h
@@ -0,0 +1,32 @@
+/**< Prototipo de la función de validación de dato ingresado */
+int ValidarDato(const char*, void*);
+
+/**< Prototipo de la función de adición */
+float SumaOperandos(float, float);
+
+/**< Prototipo de la función de sustracción */
+float RestaOperandos(float, float);
+
+/**< Prototipo de la función de división */
+float DivisionOperandos(float, float);
+
+/**< Prototipo de la función de producto */
+float MultiplicacionOperandos(float, float);
+
+/**< Prototipo de la función de factorial del primer número ingresado */
+double FactorialPrimerNumero(double);
+
+/**< Prototipo de la función de factorial del segundo número ingresado */
+double FactorialSegundoNumero(double);
+
+/**< Prototipo de validación de la primera función factorial */
+double ValidacionFactorialPrimerNumero(double);
+
+/**< Prototipo de validación de la segunda función factorial */
+double ValidacionFactorialSegundoNumero(double);
+
+/**< Prototipo de resultado de las operaciones */
+void ResultadosOperaciones(float, float);
+
+/**< Prototipo de animación de cálculo de operaciones */
+void AnimacionCalculos(double, double);
