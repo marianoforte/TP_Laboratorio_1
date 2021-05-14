3cf1e9d252bf27576b2cd6f818631c0663447723
marianoforte <marianoforte@live.com.ar>
2021-05-14 00:34:19 -0300

    Conversion a Eclipse

---
+++ TP2_ABM_Empleados/src/TP2_ABM_Empleados.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : TP2_ABM_Empleados.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include "Menu.h"
+#include "ConsoleOutputStyle.h"
+
+int main()
+{
+	setbuf(stdout,NULL);
+    system("color F0");
+    mainMenu();
+
+    return 0;
+}
